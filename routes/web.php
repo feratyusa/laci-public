6b6e449f6fd3831c11ac95853fd172a3805995ab
feratyusa <prabuega55@gmail.com>
2024-07-06 08:32:29 +0700

    init laci

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function(){
+    return User::all();
+});

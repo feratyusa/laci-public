8c83746b47606b539f3f31d058990909e24d8f72
feratyusa <prabuega55@gmail.com>
2024-08-02 14:39:51 +0700

    tiny refactor and cleanup

---
+++ resources/js/Base/FileCategory.js
@@ -0,0 +1,12 @@
+const fileCategory = [
+    {value: "SURAT USULAN", border_color: "border-orange-300", bg_color: "bg-orange-300", text_color: "text-orange-300"},
+    {value: "SURAT PERMINTAAN", border_color: "border-purple-300", bg_color: "bg-purple-300", text_color: "text-purple-300"},
+    {value: "BERITA ACARA NEGOSIASI", border_color: "border-green-300", bg_color: "bg-green-300", text_color: "text-green-300"},
+    {value: "SURAT PERINTAH KERJA", border_color: "border-teal-300", bg_color: "bg-teal-300", text_color: "text-teal-300"},
+    {value: "PERJANJIAN KERJA SAMA", border_color: "border-cyan-300", bg_color: "bg-cyan-300", text_color: "text-cyan-300"},
+    {value: "DOKUMENTASI", border_color: "border-blue-300", bg_color: "bg-blue-300", text_color: "text-blue-300"},
+    {value: "LAMPIRAN", border_color: "border-pink-300", bg_color: "bg-pink-300", text_color: "text-pink-300"},
+    {value: "OTHER", border_color: "border-rose-300", bg_color: "bg-rose-300", text_color: "text-rose-300"},
+]
+
+export default fileCategory

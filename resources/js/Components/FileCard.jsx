86d76cea7ee70b3d2454b9113f8f256a6c83f375
feratyusa <prabuega55@gmail.com>
2024-08-02 14:40:41 +0700

    feat: file show

--- resources/js/Components/FileCard.jsx
+++ resources/js/Components/FileCard.jsx
@@ -1,17 +1,36 @@
-import { Card, CardBody, CardFooter, CardHeader } from "@material-tailwind/react";
+import { DocumentTextIcon } from "@heroicons/react/24/solid";
+import { Card, CardBody } from "@material-tailwind/react";
+import { filesize } from "filesize";
+import fileCategory from "@/Base/FileCategory";
 
 export default function FileCard({file}){
+    const border_color = fileCategory.find(c => c.value === file.category).border_color
+    const bg_color = fileCategory.find(c => c.value === file.category).bg_color
+    const text_color = fileCategory.find(c => c.value === file.category).text_color
+
     return(
-        <Card>
-            <CardHeader>
-
-            </CardHeader>
+        <Card className={"w-80 border-2 "+border_color+" "+text_color}>
             <CardBody>
-
+                <div className="flex flex-col space-between">
+                    <div className="flex items-center">
+                        <DocumentTextIcon className={"max-w-32"}/>
+                        <div className="flex flex-col overflow-hidden">
+                            <p className="truncate text-base">
+                                {file.name}
+                            </p>
+                            <p className="uppercase text-base">
+                                {file.mime_type}
+                            </p>
+                            <p className="text-base">
+                                {filesize(file.size)}
+                            </p>
+                        </div>
+                    </div>
+                    <div className={"rounded-lg text-white text-center text-xs p-1 "+bg_color}>
+                        <p className="font-bold">{file.category}</p>
+                    </div>
+                </div>
             </CardBody>
-            <CardFooter>
-                
-            </CardFooter>
         </Card>
     )
 }

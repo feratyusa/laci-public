2451f73b661144f73386c4df753a6b23ad748b3a
feratyusa <prabuega55@gmail.com>
2024-08-29 09:52:10 +0700

    feat: master menu

--- resources/js/Base/Menus.js
+++ resources/js/Base/Menus.js
@@ -1,16 +1,21 @@
 const MainMenu = [
-        {name: 'Dashboard', link: 'dashboard', icon: 'home'},
-        {name: 'Usulan', link: 'proposal.index', icon: 'proposal'},
-        {name: 'Event', link: 'event.index', icon: 'event'},
-        {name: 'Kalender', link: 'calendar.index', icon: 'calendar'},
-        {name: 'Kalkulator', link: 'calculator.index', icon: 'calculator'},
-        {name: 'Tutorial', link: 'dashboard', icon: 'tutorial'}
+        {name: 'Dashboard', link: 'dashboard', url: 'home'},
+        {name: 'Usulan', link: 'proposal.index', url: 'proposals'},
+        {name: 'Event', link: 'event.index', url: 'events'},
+        {name: 'Kalender', link: 'calendar.index', url: 'calendar'},
+        {name: 'Kalkulator', link: 'calculator.index', url: 'calculator'},
+        {name: 'Master', link: [
+            {name: 'User', link: 'dashboard', url: 'master/user'},
+            {name: 'File Kategori', link: 'category.index', url: 'master/categories'},
+            {name: 'Mandatory Kategori', link: 'mandatory-category.index', url: 'master/mandatory-category'},
+        ], url: 'master'},
+        {name: 'Tutorial', link: 'dashboard', url: 'tutorial'},
     ]
 
 const ProfileMenu = [
-        {name: 'Profile', link: 'profile.edit', icon:'profile'},
-        {name: 'Settings', link: 'dashboard', icon:'settings'},
-        {name: 'Logout', link: 'logout', icon:'logout', method: 'post'}
+        {name: 'Profile', link: 'profile.edit', url:'profile'},
+        {name: 'Settings', link: 'dashboard', url:'settings'},
+        {name: 'Logout', link: 'logout', url:'logout', method: 'post'}
     ]
 
 export { MainMenu, ProfileMenu }

cef1c3e821b69a501c5fb59281ea8976ac1da0b1
feratyusa <prabuega55@gmail.com>
2024-09-07 12:36:30 +0700

    feat: command local db setup if theres no db yet

--- app/Console/Commands/LocalDBRefresh.php
+++ app/Console/Commands/LocalDBRefresh.php
@@ -7,27 +7,28 @@ use Database\Seeders\DatabaseSeeder;
 use Database\Seeders\MandatoryFileCategorySeeder;
 use Illuminate\Console\Command;
 
-class LocalDBSetup extends Command
+class LocalDBRefresh extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'local:db-setup';
+    protected $signature = 'local:db-refresh';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Setup LACI Database';
+    protected $description = 'Refresh LACI Database';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
+        $this->call('db:wipe');
         $this->call('migrate');
         $this->call('db:seed');
     }

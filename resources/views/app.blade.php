60277e9308fc9b40441d41c591659bb250ef00db
feratyusa <prabuega55@gmail.com>
2024-10-30 08:08:28 +0700

    change: web logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" type="image/x-icon" href="logo.png">
+        <link rel="icon" type="image/x-icon" href="logo.ico">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 

fbdf28d1ec71639a422117d53478b76867523ed2
Ega Prabu <68368240+feratyusa@users.noreply.github.com>
2025-01-02 11:16:54 +0700

    feat: guidebook

--- resources/js/Pages/Tutorial/Partials/TOC.jsx
+++ resources/js/Pages/Tutorial/Partials/TOC.jsx
@@ -1,12 +1,12 @@
 import { TOC } from "@/Base/TOC"
-import { BookOpenIcon } from "@heroicons/react/24/solid"
-import { Link } from "@inertiajs/react"
+import { BookOpenIcon, DocumentIcon } from "@heroicons/react/24/solid"
+import { Button } from "@material-tailwind/react"
 import { useState } from "react"
 
 export default function TableOfContents({
     setGuide
 }){
-    const [toc, ] = useState(TOC)    
+    const [toc, ] = useState(TOC)
     return(
         <div>
             <div className="flex items-center gap-5 text-red-500 mb-5">
@@ -14,33 +14,22 @@ export default function TableOfContents({
                 <p className="font-bold text-4xl">Panduan Penggunaan LACI</p>
             </div>
             <p className="text-lg mb-5">
-                LACI adalah sistem administrasi Learning Center dan sekaligus sebagai 
-                sistem untuk melakukan monitoring terhadap perkembangan pendidikan dan pelatihan yang sedang atau sudah dijalankan. 
-                <span className="font-bold"> Sangat direkomendasikan</span> bagi pengguna baru untuk membaca <span className="text-red-500 font-bold">Panduan</span> ini 
+                LACI adalah sistem administrasi Learning Center dan sekaligus sebagai
+                sistem untuk melakukan monitoring terhadap perkembangan pendidikan dan pelatihan yang sedang atau sudah dijalankan.
+                <span className="font-bold"> Sangat direkomendasikan</span> bagi pengguna baru untuk membaca <span className="text-red-500 font-bold">Panduan</span> ini
                 sebelum menggunakan fitur yang ada pada LACI.
             </p>
 
             <div className="w-full h-1 bg-red-100 mb-10 mt-10"></div>
 
             <div className="mb-10">
-                <p className="text-2xl font-bold text-red-500 mb-2">Daftar Isi</p>
-
-
-                <ol className="list-[upper-alpha] list-inside pl-5" type="A">
-                    {
-                        toc.map(content => (
-                            <Link href={`/tutorials?guide=${content.content}`}>
-                                <li 
-                                    className="cursor-pointer underline text-lg text-blue-500 hover:text-red-500 mb-1"
-                                    key={'tutorial_'+ content.title + '_li'} 
-                                >
-                                    {content.title}
-                                </li>                            
-                            </Link>
-                        ))
-                    }                    
-                </ol>
-            </div>            
+                <a href="guidebook.pdf" target="__blank">
+                    <Button color="red" className="flex items-center gap-3">
+                        <DocumentIcon className="w-8"/>
+                        PDF Panduan LACI
+                    </Button>
+                </a>
+            </div>
         </div>
     )
 }
